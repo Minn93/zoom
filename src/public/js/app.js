f876cc4c04d0f468f8cc46b7ad7b8309dc21f58a
Minn93 <tlsghktks8@gmail.com>
2021-09-28 22:39:18 +0900

    -

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+
+
+const socket= new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server✅!");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("disconnected to Server ❌!");
+});
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 1000)
