522fafed0dde1a1ea2feaa0ed1ebb9fd6432d647
Minn93 <tlsghktks8@gmail.com>
2021-09-29 21:56:59 +0900

    doing it

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,12 @@
-
-
+const messageList= document.querySelector("ul");
+const messageForm= document.querySelector("form");
 const socket= new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function handleOpen(){
     console.log("Connected to Server✅!");
-});
+}
+
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
     console.log("New Message: ", message.data);
@@ -13,6 +15,18 @@ socket.addEventListener("message", (message) => {
 socket.addEventListener("close", () => {
     console.log("disconnected to Server ❌!");
 });
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 1000)
+
+
+
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    console.log(input.value);
+    input.value = "";
+}
+
+
+messageForm.addEventListener("submit", handleSubmit);
